af4101f7816b4684a8ca282e865ed158cc893303
Mlazarovab <x24147087@student.ncirl.ie>
2025-01-29 17:19:20 +0000

    Updated script.sh with 'Job completed' message at the end

--- script.sh
+++ script.sh
@@ -11,3 +11,19 @@ touch fol_2/2_2.txt
 touch fol_2/2_3.txt
 
 rm -r fol_1 fol_2
+
+mkdir fol_1
+mkdir fol_2
+
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
+
+chmod 600 fol_1/*_2.txt
+chmod 777 fol_2/*_2.txt
+
+echo "Job completed"
